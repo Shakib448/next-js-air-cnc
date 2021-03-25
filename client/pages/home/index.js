251ac843942ae08f555a7a574dd0486f51c0d61d
Shakiba448 <shakiba448@gmail.com>
2021-03-25 15:37:01 +0600

    WIP on HomeForm

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -1,4 +1,10 @@
-import { Container, Grid, makeStyles } from "@material-ui/core";
+import {
+  Box,
+  Container,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 import Layout from "../../src/Components/Layout/Layout";
 import clsx from "clsx";
@@ -23,6 +29,9 @@ const Home = () => {
       <Navigation />
       <Layout title="Welcome to AIR-CNC">
         <Container maxWidth="lg" className={clsx(classes.root)}>
+          <Typography variant="h5">
+            <Box fontWeight="fontWeightBold">Where do you want to go</Box>
+          </Typography>
           <Grid item container direction="row" className={clsx(classes.gird)}>
             <HomeForm />
             <HomeContent />
--- client/src/Components/Home/HomeForm.js
+++ client/src/Components/Home/HomeForm.js
@@ -1,11 +1,22 @@
-import { Box, Grid, Typography } from "@material-ui/core";
+import { Grid, makeStyles, Paper } from "@material-ui/core";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: `${theme.spacing(1)}px ${theme.spacing(2)}px`,
+    width: "100%",
+    margin: `${theme.spacing(2)}px 0px`,
+    borderRadius: "10px",
+  },
+}));
 
 const HomeForm = () => {
+  const classes = useStyles();
   return (
     <Grid item container md={4} lg={4} sm={12}>
-      <Typography variant="h5">
-        <Box fontWeight="fontWeightBold">Where do you want to go</Box>
-      </Typography>
+      <Paper className={clsx(classes.paper)}>
+        <h1>Hello World</h1>
+      </Paper>
     </Grid>
   );
 };
