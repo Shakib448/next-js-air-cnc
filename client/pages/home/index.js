9b273492d71224ea7dbebc040911e59d22307e7b
Shakiba448 <shakiba448@gmail.com>
2021-03-25 12:03:39 +0600

    WIP on Home

--- client/pages/home/index.js
+++ client/pages/home/index.js
@@ -6,11 +6,10 @@ import {
   Typography,
 } from "@material-ui/core";
 import React from "react";
-import Layout from "../../src/Components/Layout";
+import Layout from "../../src/Components/Layout/Layout";
 import Link from "next/link";
 import clsx from "clsx";
-import Navigation from "../../src/Components/Navigation";
-import MobileNavigation from "../../src/Components/MobileNavigation";
+import Navigation from "../../src/Components/Navigation/Navigation";
 
 const useStyles = makeStyles((theme) => ({
   root: {
